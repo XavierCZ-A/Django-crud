7882d4910530373531bb8ff01057a3b5e00dfa00
XavierCZ <xavierkta125@gmail.com>
2023-05-20 00:39:06 -0600

    authenticate

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,5 +4,9 @@ from django.urls import path
 
 urlpatterns = [
     path('tasks/',views.Tasks, name='tasks'),
-    path('signup/',views.Signup, name='signup')
+    path('signup/',views.Signup, name='signup'),
+    path('home/',views.Home, name='home'),
+    path('logout/', views.Signout, name='logout'),
+    path('signin/',views.Signin, name='signin')
+
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse
-from django.contrib.auth import login
+from django.contrib.auth import login,logout, authenticate
 from django.db import IntegrityError
 
 def Tasks(request):
     return render(request, 'Tasks.html')
+
+
+def Home(request):
+    return render(request, 'Home.html')
+
+def Signout(request):
+    logout(request)
+    return redirect('home')
 
 def Signup(request):
     if request.method == 'GET':
@@ -19,7 +27,7 @@ def Signup(request):
                 user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
                 user.save()
                 login(request, user)
-                return render('tasks')
+                return render(request, 'Tasks.html')
             #para validar como usuarios ya creados en la BD con integrity
             except IntegrityError:
                 return render(request, 'Signup.html', {
@@ -30,3 +38,26 @@ def Signup(request):
                 'form': UserCreationForm,
                 'error': 'Password do not match'
             })
+
+def Signin(request):
+    # if request.method == 'GET':
+    #     return render(request, 'Signin.html', {
+    #         'form': AuthenticationForm,
+    #         'error': 'error singin'
+    #     })
+    # else:
+    #     print(request.POST)
+    #     return render(request, 'Signin.html', {
+    #         'form': AuthenticationForm,
+    #     })
+    if request.method == 'GET':
+        return render(request, 'signin.html', {"form": AuthenticationForm})
+    else:
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'signin.html', {"form": AuthenticationForm, "error": "Username or password is incorrect."})
+
+        login(request, user)
+        return redirect('tasks')
+
